180c44c7c8c90866cda4d94645704ee6fdd7b838
aylard82 <marco.premoli82@gmail.com>
2014-11-20 15:26:46 +0100

    Movement Script(to be refined) and WallMaterials
    Basic movement script to move and jump. Needs to be refined. New
    material to apply to walls to avoid the players being stuck into walls.

---
+++ resources/Scripts/PlayerMovement/CharacterController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+	// Use this for initialization
+	RaycastHit2D raycast;
+	float moveSpeed = 8;
+	float horMove = 0;
+	float jumpForce = 80;
+	void Start () {
+	
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		horMove = Input.GetAxis ("Horizontal");
+		rigidbody2D.velocity =new Vector2 (horMove * moveSpeed, rigidbody2D.velocity.y);
+		if (Input.GetKeyDown(KeyCode.Space) == true) {
+				rigidbody2D.AddForce(Vector2.up * jumpForce);
+		 }
+	}
+}

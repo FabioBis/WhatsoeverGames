43a5823213cf2e9bffc7dfecf6aeff43c8fbe738
FabioBis <fabio.biselli.80@gmail.com>
2014-11-22 19:50:05 +0100

    Started library implementation.

---
+++ projects/WhatsoeverUnityUtility/src/ACharacter2D.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WhatsoeverUnityUtility
+{
+    /// <summary>
+    /// This class represents an abstraction of what a character is within
+    /// the Unity environment. To use this class correctly each Unity script
+    /// attached to a character (that implements it) must extends this class.
+    /// </summary>
+    abstract public class ACharacter2D
+    {
+        // The current character healt.
+        public int Healt;
+        // The maximum speed the character can reach.
+        public float MaxSpeed;
+        // The force applied to character while moving (acceleration).
+        public float MoveForce;
+        // The force applied to character while jumping (deceleration).
+        public float JumpForce;
+
+        // The character current state (Standing, Jumping, ...).
+        protected ACharacter2DState state;
+        // The character control manager (Move, Jump, ...).
+        protected AControlManager controlManager;
+
+        /// <summary>
+        /// This method must be overidden by concrete classes, it is mandatory
+        /// to initialize the state variable. Call this method inside Awake or
+        /// Start Unity event.
+        /// </summary>
+        abstract protected void initState();
+
+        /// <summary>
+        /// This method must be overidden by concrete classes, it is mandatory
+        /// to initialize the controlManager variable. Call this method inside
+        /// Awake or Start Unity event.
+        /// </summary>
+        abstract protected void initControlManager();
+    }
+}
---
+++ projects/WhatsoeverUnityUtility/src/ACharacter2DState.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace WhatsoeverUnityUtility
+{
+    /// <summary>
+    /// This class represents an abstraction of what state a character could
+    /// be (state machine). See State Pattern.
+    /// </summary>
+    abstract public class ACharacter2DState
+    {
+        // Is the character facing right?
+        private bool facingRight;
+        // The maximum speed the character can reach.
+        private float maxSpeed;
+        // The force applied to character while moving (acceleration).
+        private float moveForce;
+        // The force applied to character while jumping (deceleration).
+        private float jumpForce;
+
+        // The character rigidbody.
+        private Rigidbody2D rigidbody;
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="horizontal"></param>
+        public void Move(float horizontal)
+        {
+            // TODO
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        public void Jump()
+        {
+            // TODO
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="weapon"></param>
+        public void Fire(IWeapon weapon)
+        {
+            // TODO
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        public void Duck()
+        { 
+            // TODO
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        public void Climb()
+        {
+            // TODO
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        protected void flip()
+        { 
+            // TODO
+        }
+    }
+}

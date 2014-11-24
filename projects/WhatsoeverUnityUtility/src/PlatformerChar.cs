a1b2573b75ac62cf2bd70cd6008d0d3bedc698d2
FabioBis <fabio.biselli.80@gmail.com>
2014-11-24 19:43:28 +0100

    From abstract to concrete Platformer Character.

---
+++ projects/WhatsoeverUnityUtility/src/PlatformerChar.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Whatsoever2DUnityUtility
+{
+    /// <summary>
+    /// This class represents an abstraction of what a character is within
+    /// the Unity environment. To use this class correctly each Unity script
+    /// attached to a character (that implements it) must extends this class.
+    /// </summary>
+    public class PlatformerChar
+    {
+        // The current character healt.
+        private int healt;
+        // The maximum speed the character can reach.
+        private float maxSpeed;
+        // The force applied to character while moving (acceleration).
+        private float moveForce;
+        // The force applied to character while jumping (deceleration).
+        private float jumpForce;
+
+        // The character current state (Standing, Jumping, ...).
+        private APlatformerCharState state;
+        // The character control manager (Move, Jump, ...).
+        private APlatformerControl controlManager;
+
+        public PlatformerChar(
+            APlatformerControl controlManager,
+            APlatformerCharState charState,
+            int h,
+            float speedMax,
+            float forceMove,
+            float forceJump
+            )
+        {
+            this.controlManager = controlManager;
+            this.state = charState;
+            this.healt = h;
+            this.maxSpeed = speedMax;
+            this.moveForce = forceMove;
+            this.jumpForce = forceJump;
+        }
+
+        // TODO
+        TODO;
+
+    }
+}
